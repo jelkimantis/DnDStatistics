a48590c1f326f3c677046db1d8dc0bb1b0c06a2a
Megan List <megan.renee.list@gmail.com>
2020-11-25 14:54:04 -0500

    classify to make each line work independently

--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-class App extends React.Component {
-  constructor(props){
+
+class Stats extends React.Component {
+  constructor(props) {
     super(props);
 
     this.state = {
-      str: "",
-      strMod: 0,
-      dex: "",
-      dexMod: 0
-    }
+      stat: "",
+      mod: ""
+    };
   }
   calculateMod = (getNumberEvent) => {
     let value = getNumberEvent.target.value;
     let modifierValue = (value-10)/2;
     this.setState({
-      str: value,
-      strMod: modifierValue,
+      stat: value,
+      mod: modifierValue,
+      name: this.props.name
     });
   }
   render(){
+    return(
+    <div>
+      <input
+      value={this.state.stat}
+      onChange={this.calculateMod}
+      />
+      <h1>I should be {this.props.name}: {this.state.mod}</h1>
+    </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  
+  render(){
     return (
       <div>
-        <input
-          value={this.state.str}
-          onChange={this.calculateMod}
-        />
-        <h1>I should be STR: {this.state.strMod}</h1>
+        <Stats name="STR" />
+        <Stats name="DEX" />
+        <Stats name="CON" />
+        <Stats name="INT" />
+        <Stats name="CHA" />
+        <Stats name="WIS" />
       </div>
     );
   }
