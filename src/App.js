698959b7139d591cc59cd5917c0e8208d4bf1a82
Megan List <megan.renee.list@gmail.com>
2020-11-25 14:24:00 -0500

    got a functioning version of STR

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+class App extends React.Component {
+  constructor(props){
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      str: "",
+      strMod: 0,
+      dex: "",
+      dexMod: 0
+    }
+  }
+  calculateMod = (getNumberEvent) => {
+    let value = getNumberEvent.target.value;
+    let modifierValue = (value-10)/2;
+    this.setState({
+      str: value,
+      strMod: modifierValue,
+    });
+  }
+  render(){
+    return (
+      <div>
+        <input
+          value={this.state.str}
+          onChange={this.calculateMod}
+        />
+        <h1>I should be STR: {this.state.strMod}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
